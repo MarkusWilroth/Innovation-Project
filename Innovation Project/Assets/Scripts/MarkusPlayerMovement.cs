b22e5249676428de3c2863a2c2d0a3b238cb4b6f
Markus Wilroth <37120955+MarkusWilroth@users.noreply.github.com>
2020-03-20 13:41:26 +0100

    Pretotype, playermovement en settings

---
+++ Innovation Project/Assets/Scripts/MarkusPlayerMovement.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarkusPlayerMovement : MonoBehaviour
+{
+    //ToDo: Flytta spelaren vänster höger, fram o tillbaka
+    //      Fixa att spelaren inte springer ut ur spelplanen
+    //      Fixa att spelaren inte kommer förlångt fram
+
+
+    public double boundLeft, boundRight, boundTop, boundDown; //hur långt en spelare kan gå
+    public float sprintSpeed, dashSpeed, dragDown;
+
+    private bool inAir;
+    private Vector3 vecMove;
+
+    public void Start()
+    {
+        vecMove = transform.position;
+        inAir = false;
+    }
+
+    public void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            
+        }
+        KeyboardMovement();
+
+        vecMove.z -= dragDown;
+        if (vecMove.z <= boundDown)
+        {
+            vecMove.z = (float)boundDown;
+        }
+        transform.position = vecMove;
+    }
+
+    private void KeyboardMovement()
+    {
+        if (!inAir)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                //Flytta framåt
+                vecMove.z += sprintSpeed;
+                if (vecMove.z >= boundTop)
+                {
+                    vecMove.z = (float)boundTop;
+                }
+            }
+
+            else if (Input.GetKey(KeyCode.S))
+            {
+                //Flytta bakåt
+                vecMove.z -= dashSpeed;
+                if (vecMove.z <= boundDown)
+                {
+                    vecMove.z = (float)boundDown;
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //Hopp
+            }
+        }
+       
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            vecMove.x -= dashSpeed;
+            if (vecMove.x <= boundLeft)
+            {
+                vecMove.x = (float)boundLeft;
+            }
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            //Flytta höger
+            vecMove.x += dashSpeed;
+            if (vecMove.x >= boundRight)
+            {
+                vecMove.x = (float)boundRight;
+            }
+        }
+
+    }
+
+}
---
+++ Innovation Project/Assets/Scripts/MarkusRoomScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarkusRoomScript : MonoBehaviour
+{
+    //ToDo: Spawn Truck, Spawn Players
+    public GameObject truckObj, playerObj; //Truck och spelarna, laddas in från unity
+    public Vector3 truckSpawnPos, playerSpawnPos; //deras spawns, laddas in från unity
+
+    private GameObject holder; //Ett obj så att man kan ändra saker i obj script efter det har spawnat
+
+    public void Start()
+    {
+        //Spawnar truck
+        holder = Instantiate(truckObj, GameObject.FindGameObjectWithTag("GameObjects").transform);
+        holder.transform.position = truckSpawnPos;
+
+        //Spawnar player
+        holder = Instantiate(playerObj, GameObject.FindGameObjectWithTag("GameObjects").transform);
+        holder.transform.position = playerSpawnPos;
+    }
+}
