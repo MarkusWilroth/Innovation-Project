03c9e9a172df7e0ad28acc309384e3c018f4e50c
Markus Wilroth <37120955+MarkusWilroth@users.noreply.github.com>
2020-03-23 09:19:58 +0100

    Updates

---
+++ Innovation Project/Assets/MarkusTruckMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarkusTruckMovement : MonoBehaviour
+{
+
+    //ToDo:
+    //Få truck att åka åt sidorna
+    //Släpa sopor och fällor
+
+    private Vector3 moveVec;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveVec = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
