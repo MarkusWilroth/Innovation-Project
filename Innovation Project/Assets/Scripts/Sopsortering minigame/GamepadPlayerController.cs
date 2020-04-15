36b94b741801cab2eaaec37c44fbd3d3954a7497
Thegemsearcher <jacklewitan@hotmail.com>
2020-04-16 01:30:44 +0200

    Update 3
    more stuff done for the minigame

--- Innovation Project/Assets/Scripts/Sopsortering minigame/GamepadPlayerController.cs
+++ Innovation Project/Assets/Scripts/Sopsortering minigame/GamepadPlayerController.cs
@@ -6,10 +6,15 @@ public class GamepadPlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     GameObject hold;
+    Rigidbody trashBody;
     public bool holding = false;
+    public float speed;
     float leftAxis;
     float forwardAxis;
-
+    public float throwForce;
+    Vector3 VelocityX;
+    Vector3 VelocityZ;
+    public Vector3 direction;
     void Start()
     {
         
@@ -21,11 +26,22 @@ public class GamepadPlayerController : MonoBehaviour
         leftAxis = Input.GetAxisRaw("JoyHorizontal");
         forwardAxis = Input.GetAxisRaw("JoyVertical");
 
-        transform.Translate(Vector3.forward * Time.deltaTime * forwardAxis);
-        transform.Translate(Vector3.right * Time.deltaTime * leftAxis);
+        transform.Translate(Vector3.forward * Time.deltaTime * forwardAxis * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * leftAxis * speed);
+
+        VelocityZ.z =  forwardAxis * speed;
+        VelocityX.x =  leftAxis * speed;
+        direction = new Vector3(VelocityX.x, 0.5f, VelocityZ.z);
+
         if (holding)
         {
             hold.transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+        }
+        if (holding && Input.GetButtonDown("X"))
+        {
+            holding = false;
+            
+            Throw();
         }
     }
 
@@ -38,5 +54,15 @@ public class GamepadPlayerController : MonoBehaviour
             collision.gameObject.transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
             collision.transform.parent = transform;
         }
+    }
+
+    private void Throw()
+    {
+        hold.transform.parent = null;
+        trashBody = hold.GetComponent<Rigidbody>();
+        trashBody.AddForce(direction * throwForce, ForceMode.Impulse);
+        
+        
+        hold = null;
     }
 }
