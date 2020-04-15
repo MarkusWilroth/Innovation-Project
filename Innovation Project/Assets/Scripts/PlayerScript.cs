80225bb7c4345f486961e21a8124786d37a1e2a5
Markus Wilroth <37120955+MarkusWilroth@users.noreply.github.com>
2020-04-16 01:35:31 +0200

    ScoreScript + PlayerScript
    ScoreScript är static

---
+++ Innovation Project/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    public string strName; //Namnet på karaktären
+    public int points, gold, playerNr; //PlayerNr är vilken spelare det är
+
+    //Något sätt att veta vilken kontroll som styr denna spelare...
+
+    //Något sätt att ha items? vilka items ska vi ha???
+
+    public void CreateCharacter(int playerNr)
+    {
+        this.playerNr = playerNr;
+        points = 0;
+        gold = 0;
+    }
+}
---
+++ Innovation Project/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreScript
+{
+    public static ScoreScript scoreScript;
+
+    public PlayerScript[] playerScripts;
+
+    public void StartWorld()
+    {
+        playerScripts = new PlayerScript[4];
+
+        for (int i = 0; i < playerScripts.Length; i++)
+        {
+            playerScripts[i].CreateCharacter(i);
+        }
+    }
+
+    public void UpdatePlayers(int playerNr, int points, int gold)
+    {
+        playerScripts[playerNr].points = points;
+        playerScripts[playerNr].gold = gold;
+    }
+}
