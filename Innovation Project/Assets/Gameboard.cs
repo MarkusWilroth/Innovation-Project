c19f6202d03e950aa6b0b2dfeb9f3f5e5908cf94
Markus Wilroth <37120955+MarkusWilroth@users.noreply.github.com>
2020-05-18 17:42:23 +0200

    Gameboard Update

---
+++ Innovation Project/Assets/Gameboard.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Step;
+
+public class Gameboard : MonoBehaviour
+{
+    public GameObject characterPrefab;
+    public int testCharacters;
+
+    private List<GameObject> characterList;
+    private GameObject[] steps;
+    private GameObject holder, startStep;
+
+    void Start()
+    {
+        characterList = new List<GameObject>();
+
+        steps = GameObject.FindGameObjectsWithTag("Step");
+        foreach (GameObject step in steps)
+        {
+            if (step.GetComponent<StepScript>().stepType == StepType.startStep)
+            {
+                startStep = step;
+                break;
+            }
+        }
+
+        if (ScoreScript.scoreScript == null) //Kommer inte från start menyn (testing)
+        {
+            for (int i = 0; i < testCharacters; i++)
+            {
+                holder = Instantiate(characterPrefab);
+                characterList.Add(holder);
+                startStep.GetComponent<StepScript>().AddCharacter(holder);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
